06df4ef4ba55edfbfc274b5d1b2f27d36a9a7390
Steimntz M. de Figueiredo <steimntz@gmail.com>
2017-08-14 21:57:01 -0400

    fixes idea problem

--- roles/xmonad/templates/xmonad.hs
+++ roles/xmonad/templates/xmonad.hs
@@ -1,9 +1,11 @@
 import XMonad
 import XMonad.Util.EZConfig
 import Config.Mappings as Cfg
+import XMonad.Hooks.SetWMName
 
 main = xmonad $ def
   { terminal    = "urxvt"
   , modMask     = mod4Mask
   , borderWidth = 2
+  , startupHook = setWMName "LG3D"
   } `additionalKeysP` Cfg.keyMappings
