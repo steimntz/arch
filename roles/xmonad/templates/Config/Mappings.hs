14c0ea787ef2ce7297c9c41b90e23340e122b049
Steimntz M. de Figueiredo <steimntz@tjro.jus.br>
2019-07-31 12:54:45 -0400

    changes scrot key

--- roles/xmonad/templates/Config/Mappings.hs
+++ roles/xmonad/templates/Config/Mappings.hs
@@ -5,4 +5,4 @@ import XMonad.Core
 
 keyMappings :: [(String, X())]
 keyMappings = [ ("M-F11", spawn "rofi -show run")
-              , ("C-<F11>", spawn "sleep 0.2; scrot -s -e 'mv $f ~/screenshots'") ]
+              , ("C-<F10>", spawn "sleep 0.2; scrot -s -e 'mv $f ~/screenshots'") ]
