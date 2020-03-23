79b32ad897895c98fb3d7992b2f4a4c0e375ec06
Steimntz M. de Figueiredo <steimntz@gmail.com>
2020-03-22 23:25:25 -0400

    adds xmobar

--- roles/xmonad/templates/xmonad.hs
+++ roles/xmonad/templates/xmonad.hs
@@ -1,13 +1,24 @@
 import XMonad
 import XMonad.Util.EZConfig
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
 import Config.Mappings as Cfg
 import Config.StartUpHooks as StartUp
 import Config.Layout
+import System.IO
 
-main = xmonad $ def
+main = do
+ xmproc <- spawnPipe "xmobar"
+ xmonad $ def
   { terminal    = "urxvt"
   , modMask     = mod4Mask
   , borderWidth = 2
+  , focusedBorderColor = "#ba9864"
   , startupHook = StartUp.hooks
-  , layoutHook  = myLayout
+  , layoutHook  = avoidStruts $ myLayout
+  , logHook     = dynamicLogWithPP xmobarPP
+                      { ppOutput = hPutStrLn xmproc
+                      , ppTitle  = xmobarColor "#4f8396" "" . shorten 50
+                      }
   } `additionalKeysP` Cfg.keyMappings
