07c0ef7b100782ff18348d7cd898d319fdd8f86f
Steimntz M. de Figueiredo <steimntz@gmail.com>
2020-06-15 18:42:02 -0400

    removes useless xscreensaver splash

--- roles/xmonad/templates/Config/StartUpHooks.hs
+++ roles/xmonad/templates/Config/StartUpHooks.hs
@@ -8,4 +8,4 @@ hooks :: X()
 hooks = do
         setWMName "LG3D"
         spawn "xinput list | egrep \"slave.*pointer\" | grep Mouse | grep -v XTEST | sed -e 's/^.*id=//' -e 's/\\s.*$//' | xargs -IMouse sh -c 'echo \"Setting mouse Mouse\"; xinput get-button-map Mouse | tr \" \" \"\n\" | sort -g | xargs | sed \"s,4 5,5 4,g\" | xargs -i sh -c \"xinput set-button-map Mouse {}; echo {};\"'"
-        spawn "/usr/bin/xscreensaver-command -exit && /usr/bin/xscreensaver &"
+        spawn "/usr/bin/xscreensaver-command -exit && /usr/bin/xscreensaver -no-splash &"
