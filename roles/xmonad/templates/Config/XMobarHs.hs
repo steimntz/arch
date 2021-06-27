aeb5f64f6c4a4b5a8a9d0bbc8ebe83cce6cacd60
Steimntz M. de Figueiredo <steimntz@gmail.com>
2021-06-27 12:38:16 -0400

    adjusts export and colors

--- roles/xmonad/templates/Config/XMobarHs.hs
+++ roles/xmonad/templates/Config/XMobarHs.hs
@@ -10,6 +10,7 @@ module Config.XMobarHs
 ) where
 
 import Data.List
+import Config.XColors as Colors
 
 data Config = Config { font         :: String
                      , additionalFonts :: [String]
@@ -46,23 +47,22 @@ cfgPairs = [ ("font"         , show.font        )
 
 config :: Config
 config = Config { font         = "-misc-fixed-*-*-*-*-10-*-*-*-*-*-*-*"
-                , additionalFonts = ["xft:DejaVuSansMono Nerd Font Mono:pixelsize=18:antialiase=true:autohinting=true", "xft:DejaVuSansMono Nerd Font Mono:pixelsize=10:antialiase=true:autohinting=true"]
-                , textOffsets = [15]
-                , bgColor      = "#000000"
-                , fgColor      = "#BFBFBF"
+                , additionalFonts = ["xft:DejaVuSansMono Nerd Font Mono:pixelsize=10:antialiase=true:autohinting=true"]
+                , textOffsets = [12]
+                , bgColor      = "#ffffff"
+                , fgColor      = "#ffffff"
                 , position     = Top
                 , lowerOnStart = True
                 , hideOnStart  = False
                 , persistent   = False
                 , border       = NoBorder
-                , borderColor  = "#BFBFBF"
+                , borderColor  = "#ffffff"
                 , commands     = [ Run $ Date "%a %b %_d %Y * %H:%M:%S" "theDate" 10, Run StdinReader]
                 , sepChar      = "%"
                 , alignSep     = "}{"
                 , template     = "%StdinReader% }{ <fc=#00FF00>%uname%</fc> * <fc=#FF0000>%theDate%</fc>"
                 }
 
--- Must be rewritten, to remove unnecessary defaults
 instance Show Config where
   showsPrec _ n s = "Config {" ++ e n ++ "}" ++ s
     where e x = intercalate ", " [g (fst y) | y <- f x, uncurry (/=) y]
@@ -123,15 +123,42 @@ data Command = Uptime                                [String] Int
              | XMonadLog
              deriving Show
 
+highlight = Colors.fromXres $ Colors.fromXres "stz.hl"
+primary = Colors.fromXres $ Colors.fromXres "stz.primary"
+secondary = Colors.fromXres $ Colors.fromXres "stz.secondary"
+background = Colors.fromXres "*background"
+foreground = Colors.fromXres "*foreground"
+blue = Colors.fromXres "*color4"
+lightblue = Colors.fromXres "*color8"
+red = Colors.fromXres "*color1"
+lightyellow = Colors.fromXres "*color11"
+
+xmobarcfg = config {  font = "xft:DejaVuSansMono Nerd Font Mono:pixelsize=12:antialiase=true:autohinting=true"
+          , additionalFonts = ["xft:DejaVuSansMono Nerd Font Mono:pixelsize=18:antialiase=true:autohinting=true", "xft:DejaVuSansMono Nerd Font Mono:pixelsize=10:antialiase=true:autohinting=true"]
+          , textOffsets = [15]
+          , border = BottomB
+          , borderColor = primary
+          , bgColor = background
+          , fgColor = foreground
+          , position = TopW L 100
+          , commands = [ Run $ Weather "CYVR" ["-t","<tempC>C","-L","18","-H","25","--normal",blue,"--high",red,"--low",lightblue] 36000
+                       , Run $ Network "enp5s0" ["-L","0","-H","32","--normal",blue,"--high",blue++"a", "--template", "<dev>: <fn=2> <tx>kB/s  <rx>kB/s</fn>", "--Low", "1000", "--High", "5000"] 10
+                       , Run $ Cpu ["-L","3","-H","50","--normal",blue,"--high",red] 10
+                       , Run $ Memory ["-t","Mem: <usedratio>%"] 10
+                       , Run $ Swap [] 10
+                       , Run $ Com "uname" ["-s","-r"] "" 36000
+                       , Run $ Date "%a %b %_d %Y %H:%M:%S" "date" 10
+                       , Run $ StdinReader
+                       ]
+          , sepChar = "%"
+          , alignSep = "}{"
+          , template = "<fc=" ++ secondary ++ ">%StdinReader%</fc>  <fn=1></fn> %cpu%  <fn=1></fn> %memory% * %swap%  <fn=1></fn> %enp5s0% }{<fc=" ++ lightyellow ++ ">%date%</fc>  <fn=1></fn> %uname%  <fn=1></fn> %CYVR% "
+          }
+
 data Run a = Run a
 
--- Must be rewritten, as derived version inserts parens
 instance Show a => Show (Run a) where
   showsPrec _ (Run x) s = "Run " ++ show x ++ s
 
--- Example of use:
--- main = export $ config { ... }
--- remember to put a "$" after every "Run"
--- otherwise, configure it the same as you would a normal .xmobarrc
-export :: Config -> IO ()
-export = writeFile ".xmobarrc".show
+export :: IO ()
+export = writeFile ".xmobarrc" $ show xmobarcfg
