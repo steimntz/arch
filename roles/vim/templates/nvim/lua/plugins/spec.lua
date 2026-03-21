356e9f9c30c425d937dbfdcbd8cfcea43d52dded
Steimntz M. de Figueiredo <steimntz@gmail.com>
2026-03-21 18:46:43 -0400

    adjusts colorscheme

--- roles/vim/templates/nvim/lua/plugins/spec.lua
+++ roles/vim/templates/nvim/lua/plugins/spec.lua
@@ -1,15 +1,6 @@
 return {
   -- the colorscheme should be available when starting Neovim
   {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
-  {
     "dstein64/vim-startuptime",
     -- lazy-load on a command
     cmd = "StartupTime",
---
+++ roles/vim/templates/nvim/lua/remap.lua
@@ -0,0 +1,5 @@
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+vim.keymap.set("n", "<leader>fg", builtin.git_files, { desc = "Find git files" })
+vim.keymap.set("n", "<leader>fl", builtin.live_grep, { desc = "Live grep" })
